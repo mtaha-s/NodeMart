e66195e5a6610e8d8d24a509e13c0bd3a29fa827
Muhammad Taha <mtaha.shahid@systemsltd.com>
2026-02-12 12:37:38 +0500

    Edited in inventorySchema

--- src/models/inventorySchema.js
+++ src/models/inventorySchema.js
@@ -4,13 +4,13 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const inventorySchema = new mongoose.Schema({
   itemCode:         { type: String, required: true, unique: true, index: true },
   itemName:         { type: String, required: true },
-  itemdescription:  { type: String },
-  itemcategory:     { type: String },
+  description:  { type: String },
+  category:     { type: String },
   vendorId:         { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor', required: true },
   quantity:         { type: Number, required: true },
-  itemCostPrice:    { type: Number, required: true },
-  itemRetailPrice:  { type: Number, required: true },
-  itemreorderLevel: { type: Number, default: 10 },
+  costPrice:    { type: Number, required: true },
+  retailPrice:  { type: Number, required: true },
+  reorderLevel: { type: Number, default: 10 },
   imageUrl:         { type: String }, // store URL pointing to image in storage
 }, { timestamps: true });
 
