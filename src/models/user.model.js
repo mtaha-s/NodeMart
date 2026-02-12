9cdd03930b7257cc4acfcf6de5f5fe770743d19f
Muhammad Taha <mtaha.shahid@systemsltd.com>
2026-02-12 15:29:38 +0500

    passwordHash to password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,7 +3,7 @@ import mongoose, {Schema} from 'mongoose';
 const userSchema = new mongoose.Schema({
     fullName:       { type: String, required: true, trim: true, index: true },
     email:          { type: String, required: true, unique: true, lowecase: true, trim: true },
-    passwordHash:   { type: String, required: [true, 'Password is required'] },
+    password:       { type: String, required: [true, 'Password is required'] },
     avatar:         { type: String },
     role:           { type: String, enum: ['admin', 'staff', 'user', 'vendor'], default: 'user' },
     isActive:       { type: Boolean, default: true },
