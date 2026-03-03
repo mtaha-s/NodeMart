18344a09880da182b6efe7fd7ccea3325d70f7f5
Muhammad Taha <mtaha.shahid@systemsltd.com>
2026-03-03 12:33:56 +0500

    Indendation

--- src/models/inventory.model.js
+++ src/models/inventory.model.js
@@ -3,16 +3,17 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 // Inventory Schema
 const inventorySchema = new mongoose.Schema({
-  itemCode:     { type: String, required: true, unique: true, index: true },
-  itemName:     { type: String, required: true },
-  description:  { type: String },
-  category:     { type: String },
-  vendorId:     { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor', required: true },
-  quantity:     { type: Number, required: true },
-  costPrice:    { type: Number, required: true },
-  retailPrice:  { type: Number, required: true },
-  reorderLevel: { type: Number, default: 10 },
-  imageUrl:     { type: String }, // store URL pointing to image in storage
+  itemCode:        { type: String, required: true, unique: true, index: true },
+  itemName:        { type: String, required: true },
+  description:     { type: String },
+  category:        { type: String },
+  vendorId:        { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor', required: true, index: true },
+  vendorProductId: {type: mongoose.Schema.Types.ObjectId, ref: "VendorProduct", required: true,},
+  quantity:        { type: Number, required: true },
+  costPrice:       { type: Number, required: true },
+  retailPrice:     { type: Number, required: true },
+  reorderLevel:    { type: Number, default: 10 },
+  imageUrl:        { type: String }, // store URL pointing to image in storage
 }, { timestamps: true });
 
 // Add pagination plugin
